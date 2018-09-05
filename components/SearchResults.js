8633f6a236db721b3524ff92e94a808b7fea5e28
John Vascsinec <pensteeler@gmail.com>
2018-09-05 23:13:39 +0000

    automatically backed up by learn

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,6 +4,14 @@ import React from 'react';
 
 const SearchResults = ({ results }) => (
   <ul>
+    {
+      results.map(({ title, description, link }, i) => (
+        <li key={i}>
+          <a href={link}>{title}</a>
+          <p>{description}</p>
+        </li>
+      ))
+    }
   </ul>
 );
 
